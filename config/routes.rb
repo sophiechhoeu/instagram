9479090187dafa8d1c243de2da041a2a7744f5a7
Sophie Chhoeu <sophie.chhoeu@gmail.com>
2017-10-04 16:29:28 +1100

    shrine add-ons

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'landing#index'
+  resources :photos
+  root 'home#landing'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171004041050) do
+ActiveRecord::Schema.define(version: 20171004044922) do
+
+  create_table "photos", force: :cascade do |t|
+    t.text "image_data"
+    t.integer "user_id"
+    t.string "caption"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_photos_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
