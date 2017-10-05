d902a9d24e5a700326c5a5460be5053c81a01764
Sophie Chhoeu <sophie.chhoeu@gmail.com>
2017-10-05 14:00:54 +1100

    more shit

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -70,6 +70,6 @@ class PhotosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def photo_params
-      params.require(:photo).permit(:image_data, :user_id, :caption)
+      params.require(:photo).permit(:image, :user_id, :caption)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :photos, :new, :create, :index, :edit, :update
+  resources :photos
   root 'home#landing'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
